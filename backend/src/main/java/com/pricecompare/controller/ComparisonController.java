60d33bd51a88fccf6bf79e5296280c4678347301
Vedvart98 <yadavvedvart@gmail.com>
2026-06-07 13:01:22 +0530

    updated cors config

--- backend/src/main/java/com/pricecompare/controller/ComparisonController.java
+++ backend/src/main/java/com/pricecompare/controller/ComparisonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/compare")
-@CrossOrigin(origins = {"http://localhost:4200", "${allowed.origins:}"})
+// @CrossOrigin(origins = {"http://localhost:4200", "${allowed.origins:}"})
 @Slf4j
 @RequiredArgsConstructor
 public class ComparisonController {
